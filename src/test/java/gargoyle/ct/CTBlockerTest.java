335556da975bd19e16a1789ccba2103f3f9bdb17
Gargoyle <lure.of.chaos@gmail.com>
2016-06-08 00:08:46 +0300

    blocker test

--- src/test/java/gargoyle/ct/CTBlockerTest.java
+++ src/test/java/gargoyle/ct/CTBlockerTest.java
@@ -3,14 +3,15 @@ package gargoyle.ct;
 import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
-import org.junit.After;
-import org.junit.Before;
-
 public class CTBlockerTest implements MessageProvider {
 	public static void main(final String[] args) throws Exception {
 		final CTBlockerTest test = new CTBlockerTest();
-		test.setUp();
-		test.testBlocker();
+		test.messages = ResourceBundle.getBundle("messages");
+		test.blocker = new CTBlocker(test);
+		test.blocker.debug(true);
+		test.blocker.setText("00:00");
+		test.blocker.setVisible(true);
+		test.blocker.toFront();
 	}
 
 	private CTBlocker blocker;
@@ -20,26 +21,5 @@ public class CTBlockerTest implements MessageProvider {
 	public String getMessage(final String message, final Object... args) {
 		assert this.messages != null;
 		return MessageFormat.format(this.messages.getString(message), args);
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		this.messages = ResourceBundle.getBundle("messages");
-		this.blocker = new CTBlocker(this);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		this.blocker = null;
-	}
-
-	// @Test
-	public void testBlocker() {
-		if (this.blocker != null) {
-			this.blocker.debug(true);
-			this.blocker.setText("00:00");
-			this.blocker.setVisible(true);
-			this.blocker.toFront();
-		}
 	}
 }
