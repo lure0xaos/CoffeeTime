23ed6a6406ef2e2e3e39d965ee87836b4894b56f
Gargoyle <lure.of.chaos@gmail.com>
2016-06-07 22:52:24 +0300

    logtest

--- src/test/java/gargoyle/ct/CTLogTest.java
+++ src/test/java/gargoyle/ct/CTLogTest.java
@@ -1,10 +1,39 @@
 package gargoyle.ct;
 
+import java.util.LinkedList;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CTLogTest {
+	LinkedList<LogRecord> records;
+
+	@Before
+	public void setUp() {
+		this.records = new LinkedList<LogRecord>();
+		Logger.getLogger(CTLogTest.class.getName()).addHandler(new Handler() {
+			@Override
+			public void close() throws SecurityException {
+			}
+
+			@Override
+			public void flush() {
+			}
+
+			@Override
+			public void publish(final LogRecord record) {
+				CTLogTest.this.records.add(record);
+			}
+		});
+	}
+
 	@Test
 	public void testLog() {
 		Log.warn("test");
+		Assert.assertEquals(CTLogTest.class.getName(), this.records.getLast().getSourceClassName());
 	}
 }
