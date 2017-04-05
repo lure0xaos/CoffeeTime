93e123b32b03f06a7d93c4c7d7d0ce32268a830a
Lure of Chaos <lure.of.chaos@gmail.com>
2017-04-05 13:09:52 +0300

    parent messages

--- src/main/java/gargoyle/ct/messages/impl/CTMessages.java
+++ src/main/java/gargoyle/ct/messages/impl/CTMessages.java
@@ -2,22 +2,34 @@ package gargoyle.ct.messages.impl;
 
 import gargoyle.ct.messages.LocaleProvider;
 import gargoyle.ct.messages.MessageProvider;
+import gargoyle.ct.util.Log;
 
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.ResourceBundle.Control;
 
 public class CTMessages implements MessageProvider {
+    private static final String MSG_NO_BUNDLE = "Can''t find bundle {0}";
     private static final String MSG_NO_MESSAGE = "Can''t find resource for bundle {0}, key {1}";
     private static final String CAN_T_PARSE_MESSAGE_0_1_2 = "can''t parse message:{0}->{1}({2})";
     private final LocaleProvider localeProvider = new LocaleProvider();
+    private final MessageProvider parent;
     private ResourceBundle messages;
 
     public CTMessages(String baseName) {
-        this(baseName, Locale.getDefault());
+        this(null, baseName, Locale.getDefault());
     }
 
-    private CTMessages(String baseName, Locale locale) {
+    public CTMessages(String baseName, Locale locale) {
+        this(null, baseName, locale);
+    }
+
+    public CTMessages(MessageProvider parent, String baseName) {
+        this(parent, baseName, Locale.getDefault());
+    }
+
+    public CTMessages(MessageProvider parent, String baseName, Locale locale) {
+        this.parent = parent;
         localeProvider.setLocale(locale);
         load(baseName);
     }
@@ -49,16 +61,30 @@ public class CTMessages implements MessageProvider {
                         MessageFormat.format(CAN_T_PARSE_MESSAGE_0_1_2, message, pattern, Arrays.toString(args)), ex);
             }
         } catch (MissingResourceException ex) {
-            String key = ex.getKey();
-            String bundle = messages.getBaseBundleName();
-            throw new MissingResourceException(MessageFormat.format(MSG_NO_MESSAGE, bundle, key), bundle, key);
+            if (parent == null) {
+                String key = ex.getKey();
+                String bundle = messages.getBaseBundleName();
+                throw new MissingResourceException(MessageFormat.format(MSG_NO_MESSAGE, bundle, key), bundle, key);
+            } else {
+                return parent.getMessage(message, args);
+            }
         }
     }
 
     private void load(String baseName) {
-        messages =
-                ResourceBundle.getBundle(baseName, localeProvider.getLocale(),
-                        Control.getControl(Control.FORMAT_PROPERTIES));
+        try {
+            messages =
+                    ResourceBundle.getBundle(baseName, localeProvider.getLocale(),
+                            Control.getControl(Control.FORMAT_PROPERTIES));
+        } catch (MissingResourceException ex) {
+            if (parent == null) {
+                String key = ex.getKey();
+                String bundle = messages.getBaseBundleName();
+                throw new MissingResourceException(MessageFormat.format(MSG_NO_BUNDLE, bundle), bundle, key);
+            } else {
+                Log.warn(MessageFormat.format(MSG_NO_BUNDLE, messages.getBaseBundleName()));
+            }
+        }
     }
 
     private void reload() {
