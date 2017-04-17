222c92cef22de3e949495d371c2e349de2c1f0ea
Gargoyle <lure.of.chaos@gmail.com>
2017-04-17 16:32:44 +0300

    pref

---
+++ src/main/java/gargoyle/ct/messages/impl/CTComponentLocaleProvider.java
@@ -0,0 +1,37 @@
+package gargoyle.ct.messages.impl;
+
+import gargoyle.ct.messages.LocaleProvider;
+import gargoyle.ct.pref.CTPreferences.SUPPORTED_LOCALES;
+import gargoyle.ct.prop.CTObservableProperty;
+import gargoyle.ct.ui.prop.CTComponentLocaleProperty;
+
+import java.awt.Component;
+import java.util.Locale;
+
+public final class CTComponentLocaleProvider implements LocaleProvider {
+
+    private final CTObservableProperty<SUPPORTED_LOCALES> locale;
+
+    public CTComponentLocaleProvider(Component component) {
+        this(new CTComponentLocaleProperty(component));
+    }
+
+    private CTComponentLocaleProvider(CTObservableProperty<SUPPORTED_LOCALES> locale) {
+        this.locale = locale;
+    }
+
+    @Override
+    public Locale getLocale() {
+        return locale.get().getLocale();
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+        this.locale.set(SUPPORTED_LOCALES.forLocale(locale));
+    }
+
+    @Override
+    public CTObservableProperty<SUPPORTED_LOCALES> locale() {
+        return locale;
+    }
+}
---
+++ src/main/java/gargoyle/ct/ui/prop/CTComponentLocaleProperty.java
@@ -0,0 +1,26 @@
+package gargoyle.ct.ui.prop;
+
+import gargoyle.ct.pref.CTPreferences.SUPPORTED_LOCALES;
+import gargoyle.ct.prop.impl.CTBaseObservableProperty;
+
+import java.awt.Component;
+
+public class CTComponentLocaleProperty extends CTBaseObservableProperty<SUPPORTED_LOCALES> {
+
+    private final Component component;
+
+    public CTComponentLocaleProperty(Component component) {
+        super(component.getName());
+        this.component = component;
+    }
+
+    @Override
+    public SUPPORTED_LOCALES get() {
+        return SUPPORTED_LOCALES.findSimilar(component.getLocale(), SUPPORTED_LOCALES.findSimilar());
+    }
+
+    @Override
+    public void set(SUPPORTED_LOCALES value) {
+        component.setLocale(value.getLocale());
+    }
+}
